79693aae81562a3ebbc4837f30ba404a12ac9e28
Marcus <mcihme@gmail.com>
2024-12-16 14:13:22 +0100

    Debug

--- client/src/api.js
+++ client/src/api.js
@@ -2,6 +2,7 @@ import axios from 'axios';
 
 // const API_BASE_URL = 'http://localhost:5000'; 
 const API_BASE_URL = process.env.REACT_APP_API_BASE_URL;
+console.log('API_BASE_URL:', API_BASE_URL);
 
 export const registerUser = async (userData) => {
     try {
